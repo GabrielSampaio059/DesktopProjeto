0d47126310fa10ad8bb2695ece138700e1ca142f
unknown <d2022004467@unifei.edu.br>
2022-10-06 10:47:29 -0300

    Adcionei os serviços(deletar/adicionar/atualizar/mostrar todos/pesquisar livro) de forma semelhante ao crud

--- src/app/shared/services/crud.service.ts
+++ src/app/shared/services/crud.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
-
+import { Book } from '../services/livro';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CrudService {
-
-  constructor() { }
+  BooksRef: AngularFireList<any>;
+  BookRef: AngularFireObject<any>;
+  constructor(private db: AngularFireDatabase) {}
+  // Create Student
+  AddBook(Book: Book) {
+    this.BooksRef.push({
+      id:Book.id,
+      name:Book.name,
+      value:Book.value,
+      description:Book.description,
+    });
+  }
+  // Fetch Single Student Object
+  GetBook(id: string) {
+    this.BookRef = this.db.object('Book-list/' + id);
+    return this.BookRef;
+  }
+  // Fetch Students List
+  GetBookList() {
+    this.BooksRef = this.db.list('Books-list');
+    return this.BooksRef;
+  }
+  // Update Student Object
+  UpdateBook(Book: Book) {
+    this.BookRef.update({
+      
+        id:Book.id,
+        name:Book.name,
+        value:Book.value,
+        description:Book.description,
+    });
+  }
+  // Delete Student Object
+  DeleteBook(id: string) {
+    this.BookRef = this.db.object('students-list/' + id);
+    this.BookRef.remove();
+  }
 }
