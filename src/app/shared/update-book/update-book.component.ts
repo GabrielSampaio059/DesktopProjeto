adae623f53207dfd482a8fd85c414b4228c4cb25
unknown <d2022004467@unifei.edu.br>
2022-10-08 22:07:31 -0300

    componente de atualização de livro adicionada, porem sem elementos de front(atualizada)

---
+++ src/app/shared/update-book/update-book.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CrudService } from '../services/crud.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+@Component({
+  selector: 'app-book-student',
+  templateUrl: './edit-book.component.html',
+  styleUrls: ['./edit-book.component.scss'],
+})
+export class EditBookComponent implements OnInit {
+  editForm: FormGroup;
+  constructor(
+    private crudApi: CrudService,
+    private fb: FormBuilder,
+    private location: Location,
+    private actRoute: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit() {
+    this.updateStudentData();
+    const id = this.actRoute.snapshot.paramMap.get('id');
+    this.crudApi
+      .GetBook(id)
+      .valueChanges()
+      .subscribe((data) => {
+        this.editForm.setValue(data);
+      });
+  }
+  get BookId() {
+    return this.editForm.get('id');
+  }
+  get BookName() {
+    return this.editForm.get('name');
+  }
+  get Bookdescription() {
+    return this.editForm.get('description');
+  }
+  get Bookvalue() {
+    return this.editForm.get('value');
+  }
+  updateStudentData() {
+    this.editForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      id: ['',[Validators.required, Validators.pattern('^[0-9]+$')]],
+      description: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+$'),
+        ],
+      ],
+      value: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+    });
+  }
+  goBack() {
+    this.location.back();
+  }
+  updateForm() {
+    this.crudApi.UpdateBook(this.editForm.value);
+    this.toastr.success(
+      this.editForm.controls['name'].value + ' atualizado com sucesso!!'
+    );
+    this.router.navigate(['view-students']);
+  }
+}
