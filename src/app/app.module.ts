b3cbe556e7513bfb45f52c16d5c4fee977dee216
unknown <d2022004467@unifei.edu.br>
2022-10-08 22:25:40 -0300

    correção do codigo de imports de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './shared/add-book/add-book.component';
+import { EditBookComponent } from './shared/update-book/update-book.component';
+import { BookListComponent } from './shared/list-book/list-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    EditBookComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
