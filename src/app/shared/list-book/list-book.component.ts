09e82c31a7d8b1347b46ed3daedb193ee9c07c9f
unknown <d2022004467@unifei.edu.br>
2022-10-08 21:42:23 -0300

    componente de listagem de livro adicionada, porem sem elementos de front

---
+++ src/app/shared/list-book/list-book.component.ts
@@ -0,0 +1,54 @@
+
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../services/crud.service';
+import { Book } from '../services/livro'; 
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.scss']
+})
+export class StudentListComponent implements OnInit {
+  p: number = 1;
+  Student: Book[];
+  hideWhenNoStudent: boolean = false;
+  noData: boolean = false;
+  preLoader: boolean = true;
+  
+  constructor(
+    public crudApi: CrudService,
+    public toastr: ToastrService
+    ){ }
+
+  ngOnInit() {
+    this.dataState();
+    let s = this.crudApi.GetBookList(); 
+    s.snapshotChanges().subscribe(data => {
+      this.Student = [];
+      data.forEach(item => {
+        let a = item.payload.toJSON(); 
+        a['$key'] = item.key;
+        this.Student.push(a as Book);
+      })
+    })
+  }
+  dataState() {     
+    this.crudApi.GetBookList().valueChanges().subscribe(data => {
+      this.preLoader = false;
+      if(data.length <= 0){
+        this.hideWhenNoStudent = false;
+        this.noData = true;
+      } else {
+        this.hideWhenNoStudent = true;
+        this.noData = false;
+      }
+    })
+  }
+  deleteStudent(book) {
+    if (window.confirm('Tem certeza que deseja deletar esse livro ?')) { 
+      this.crudApi.DeleteBook(book.$key)
+      this.toastr.success(book.name + ' deletado com sucesso!');
+    }
+  }
+}
